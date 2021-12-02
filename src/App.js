00517193601fa9dde70bd5a27e9ac97d0ddf0016
Axe-Pearl <mycoursesbattletour@gmail.com>
2021-12-03 02:18:17 +0530

    Created Card Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-
+import Card from "./components/Card/Card";
 function App() {
   return (
     <div>
-      <h2>Eco Bazar</h2>
+      {/* <h2>Eco Bazar</h2> */}
+      <Card />
     </div>
   );
 }
