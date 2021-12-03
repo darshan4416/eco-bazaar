789ae357203bd530214168cdfac7d6fb900dcb5b
Axe-Pearl <mycoursesbattletour@gmail.com>
2021-12-03 16:09:07 +0530

    Added more Products

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,28 +1,35 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import "./Card.css";
 import Products from '../productsdata';
 function Card() {
     return (
-        <>
- <div className="wrapper">
-      <div className="product-img">
-      <img src="https://www.ecohoy.com/media/catalog/product/cache/1/small_image/350x/9df78eab33525d08d6e5fb8d27136e95/P/a/PartyPack11000x1000.jpg
-       " height="420" width="327" />
-     </div>
-    <div className="product-info">
-      <div className="product-text">
-        <h1>Harvest Vase</h1>
-        <h2>by studio and friends</h2>
-        <p>Harvest Vases are a reinterpretation<br /> of peeled fruits and vegetables as<br /> functional objects. The surfaces<br /> appear to be sliced and pulled aside,<br /> allowing room for growth. </p>
-      </div>
-      <div className="product-price-btn">
-        <p><span>78</span>$</p>
-        <button type="button">buy now</button>
-      </div>
-    </div>
-  </div>
+        <Fragment className="ParentCard">
+        {
+          Products.map((Product)=>{
+            return(
+              <div className="wrapper">
+              <div className="product-img">
+              <img src="https://www.ecohoy.com/media/catalog/product/cache/1/small_image/350x/9df78eab33525d08d6e5fb8d27136e95/P/a/PartyPack11000x1000.jpg
+               " height="420" width="327" />
+             </div>
+            <div className="product-info">
+              <div className="product-text">
+                <h1>{Product.ProductName}</h1>
+                <h2>by studio and friends</h2>
+                <p>Lorem isdasasdfsjd <br /> of peeled fruits and vegetables as<br /> functional objects. The surfaces<br /> appear to be sliced and pulled aside,<br /> allowing room for growth. </p>
+              </div>
+              <div className="product-price-btn">
+                <p><span>{Product.price}</span>$</p>
+                <button type="button">buy now</button>
+              </div>
+            </div>
+          </div>
+            )
+          })
+        }
+ 
 
-        </>
+        </Fragment>
     )
 }
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,12 +5,13 @@ function Navbar() {
     return (
         <>
     <header className="header">
-	    <h1 className="logo"><a href="#">Flexbox</a></h1>
+	    <h1 className="logo"><a href="#">Sustainable Waves</a></h1>
         <ul className="main-nav">
           <li><a href="#">Home</a></li>
+          <li><a href="#">Explore</a></li>
+          <li><a href="#">Login</a></li>
+          <li><a href="#">Register</a></li>
           <li><a href="#">About</a></li>
-          <li><a href="#">Portfolio</a></li>
-          <li><a href="#">Contact</a></li>
         </ul>
 	</header> 
 
